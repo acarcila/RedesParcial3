07526b528fdd8831a495ba3012bf934e164b27e9
acarcila <acarcila98@gmail.com>
2019-11-17 23:39:46 -0500

    Se modificaron los nombres de los archivos

--- TypescriptAPI/src/index.ts
+++ TypescriptAPI/src/index.ts
@@ -1,27 +1,27 @@
-"use strict";
-exports.__esModule = true;
 // Cargar modulos y crear nueva aplicacion
-var express = require("express");
+import * as express from "express";
 var app = express();
-var cors = require("cors");
-var bodyParser = require("body-parser");
+import * as cors from "cors";
+import * as bodyParser from "body-parser";
+
 app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
 app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
 app.use(cors());
-//Ejemplo: GET http://localhost:8080
-app.get("/suma/:var1/:var2", function (req, res, next) {
-    var var1 = parseFloat(req.params.var1);
-    var var2 = parseFloat(req.params.var2);
-    var suma = var1 + var2;
+
+app.get("/suma/:var1/:var2", function (req, res) {
+    var var1: number = parseFloat(req.params.var1);
+    var var2: number = parseFloat(req.params.var2);
+    var suma: number = var1 + var2;
     res.json(suma);
 });
-//Ejemplo: POST http://localhost:8080/insert/estudiante
+
 app.post("/suma", function (req, res) {
     var body = req.body;
     var suma = body.var1 + body.var2;
     res.json(suma);
     console.log(suma);
 });
+
 var server = app.listen(8080, function () {
     console.log("Server is running..");
 });
